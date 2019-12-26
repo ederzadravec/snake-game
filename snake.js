4a3f39d52f8f7958ba697fac4be0485a73c211c9
Éder Zadravec <ederzadravec@gmail.com>
2019-12-26 12:19:18 -0300

    fazendo a cobrinha crescer

--- snake.js
+++ snake.js
@@ -1,134 +1,175 @@
 function init() {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	var cw = 10;
-	var direction = 'right';
-	var snakeX = 1, snakeY = 1, appleX, appleY;
-	var score = 0;
-	var time = 0;
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var cw = 10;
+  var direction = "right";
+  var appleX, appleY;
+  var score = 0;
+  var time = 0;
 
-	createBoard();
-	createApple();
-	createSnake();
+  var snake = [[0, 0]];
 
-	var i1 = setInterval(paint, 60);
-	var i2 = setInterval(snake, 100);
-	var i3 = setInterval(updateTime, 1000);
+  createBoard();
+  createApple();
+  paintSnake();
 
-	function updateTime()
-	{
-		time ++;
-		document.getElementById('time').innerHTML = "time: " + time;
-	}
+  var i1 = setInterval(paint, 60);
+  var i2 = setInterval(snakeFunction, 100);
+  var i3 = setInterval(updateTime, 1000);
 
-	function createSnake()
-	{
-		board[snakeX][snakeY] = 'C';
-	}
+  function updateTime() {
+    time++;
+    document.getElementById("time").innerHTML = "time: " + time;
+  }
 
-	function colisao()
-	{
-		document.writeln('GAME OVER');
-		
-		clearInterval(i1);
-		clearInterval(i2);
-		clearInterval(i3);
-		
-	}
+  function paintSnake(X = 'C') {
+    snake.forEach(([snakeX, snakeY]) => {
+      board[snakeX][snakeY] = X;
+    });
+  }
 
-	function snake()
-	{
-		board[snakeX][snakeY] = '';
+  function colisao() {
+    document.writeln("GAME OVER");
 
-		switch (direction) {
-			case 'right':
-				if (snakeX === 19) {	
-					colisao();
-					snakeX = 0;
-				}else{
-					snakeX += 1;	
-				}
-				break;
+    clearInterval(i1);
+    clearInterval(i2);
+    clearInterval(i3);
+  }
 
-			case 'left':
-				if (snakeX === 0){
-					colisao();
-					snakeX = 19;
-				}else 	{
-			 		snakeX -= 1;	
-				}
-			 	break;
+  function snakeFunction() {
+    var [snakeX, snakeY] = snake[0];
 
-			case 'up': 	
-				if (snakeY === 0){ 	
-					colisao();
-					snakeY = 19;
-				}else{
-					snakeY -= 1;	
-				}
-				break;
-			
-			case 'down': 	
-				if (snakeY === 19) {	
-					colisao();
-					snakeY = 0;
-				}else{
-					snakeY += 1;	break;
-				}
-		}		
+    paintSnake('')
 
+    const boardLength = board.length - 1;
 
-		board[snakeX][snakeY] = 'C';
+    if (snake.find(([x, y]) => x === snakeX && y === snakeY)) {
+      return colisao()
+    }
 
-		if (appleX == snakeX && appleY == snakeY){
-			createApple();
-		}
-	}
+    switch (direction) {
+      case "right":
+        if (snakeX === boardLength) {
+          colisao();
+          snakeX = 0;
+        } else {
+          snakeX += 1;
+        }
+        break;
 
-	onkeydown = function(e) {
-		var key = e.keyCode;
-		if (key == 37 && direction != "right")	direction = "left";
-		else if (key == 38 && direction != "down")	direction = "up";
-		else if (key == 39 && direction != "left")	direction = "right";
-		else if (key == 40 && direction != "up")	direction = "down";
-	}
+      case "left":
+        if (snakeX === 0) {
+          colisao();
+          snakeX = boardLength;
+        } else {
+          snakeX -= 1;
+        }
+        break;
 
-	function createBoard()
-	{
-		board = new Array(20);
-		for (var i = 0; i < 20; i++) {
-			board[i] = new Array(20);
-		}
-	}
+      case "up":
+        if (snakeY === 0) {
+          colisao();
+          snakeY = boardLength;
+        } else {
+          snakeY -= 1;
+        }
+        break;
 
-	function createApple()
-	{
-		appleX = Math.floor(Math.random() * (19 - 0 + 1));
-		appleY = Math.floor(Math.random() * (19 - 0 + 1));
-		board[appleX][appleY] = 'M';
-		score ++;
-		document.getElementById('score').innerHTML = "score: " + score;
-	}
+      case "down":
+        if (snakeY === boardLength) {
+          colisao();
+          snakeY = 0;
+        } else {
+          snakeY += 1;
+          break;
+        }
+    }
 
-	function paintCell(x, y, color) 
-	{
-		ctx.fillStyle = color || "white";
-		ctx.fillRect(x * cw, y * cw, cw, cw);
-		ctx.strokeStyle = "#f5f5f5";
-		ctx.strokeRect(x * cw, y * cw, cw, cw);
-	}
+    snake.pop();
+    snake.unshift([snakeX, snakeY ]);
 
-	function paint()
-	{
-		for (var i = 0; i < board.length; ++i) {
-			for (var j = 0; j < board.length; ++j) {
-				switch(board[i][j]) {
-					case 'M': 	paintCell(i, j, 'red'); 		break;
-					case 'C': 	paintCell(i, j, 'green'); 		break;
-					default: 	paintCell(i, j);				break;
-				}
-			}
-		}
+    if (appleX == snakeX && appleY == snakeY) {
+      increaseSnake(appleX, appleY);
+      createApple();
+    }
+  }
 
-	}
+  function increaseSnake(x, y) {
+    var newX = x;
+    var newY = y;
+
+    if (direction === "right") {
+      newX = x + 1;
+    }
+
+    if (direction === "top") {
+      newY = y - 1;
+    }
+
+    if (direction === "left") {
+      newX = x - 1;
+    }
+
+    if (direction === "bottom") {
+      newY = y + 1;
+    }
+
+    snake.push([newX, newY]);
+  }
+
+  onkeydown = function(e) {
+    var key = e.keyCode;
+    if (key == 37 && direction != "right") direction = "left";
+    else if (key == 38 && direction != "down") direction = "up";
+    else if (key == 39 && direction != "left") direction = "right";
+    else if (key == 40 && direction != "up") direction = "down";
+  };
+
+  function createBoard() {
+    board = new Array(50);
+    for (var i = 0; i < 50; i++) {
+      board[i] = new Array(50);
+    }
+  }
+
+  function createApple() {
+
+    const boardLength = board.length - 1;
+
+    do {
+      appleX = Math.floor(Math.random() * (boardLength));
+      appleY = Math.floor(Math.random() * (boardLength));
+    } while(snake.find(([x, y]) => x === appleX && y === appleY))
+
+    score++;
+    document.getElementById("score").innerHTML = "score: " + score;
+  }
+
+  function paintCell(x, y, color) {
+    ctx.fillStyle = color || "white";
+    ctx.fillRect(x * cw, y * cw, cw, cw);
+    ctx.strokeStyle = "#f5f5f5";
+    ctx.strokeRect(x * cw, y * cw, cw, cw);
+  }
+
+  function paint() {
+    paintSnake();
+    board[appleX][appleY] = "M";
+
+    for (var i = 0; i < board.length; ++i) {
+      for (var j = 0; j < board.length; ++j) {
+        switch (board[i][j]) {
+          case "M":
+            paintCell(i, j, "red");
+            break;
+          case "C":
+            paintCell(i, j, "green");
+            break;
+          default:
+            paintCell(i, j);
+            break;
+        }
+      }
+    }
+  }
 }
